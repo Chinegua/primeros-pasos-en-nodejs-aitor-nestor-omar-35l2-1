2ff7ff27a31b8d59126fae12c38f9fb02871c964
Nestor <alu0100812503@ull.edu.es>
2017-02-12 19:56:08 +0000

    Arreglando error

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.set('port',(process.env.PORT || 8080));
 app.use(express.static(__dirname + '/_book'))
 
 app.listen(app.get('port'), () => {
-  console.log(`Node app is running at localhost: $(app.get('port')}`);
+    console.log(`Node app is running at localhost: ${app.get('port')}` );
 });
