a741cdc8a494a913dde9ba48eb9db7961c50045f
Aitor Bernal <alu0100830064@ull.edu.es>
2017-02-18 23:28:50 +0000

    Añadiendo mejora contributors

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -1,13 +1,30 @@
-var exec = require('child_process').exec
-var mod = require('fs')
+let exec = require('child_process').exec
+let mod = require('fs')
 
-child =exec ('git log',function(error,stdout,stderr){
-  console.log('stdout: ' + stdout)
-  mod.writeFile("../MAINTAINERS", stdout, function(err){
+child =exec ('sh git-log.sh',function(error,stdout,stderr){
+  //console.log('stdout: ' + stdout)
+  //console.log(stdout)
+  
+  mod.writeFile("contributors.json", stdout, function(err){
       if(err){
         return console.log(err);
       }
-      console.log("Modified file");
+      let content = mod.readFileSync("contributors.json");
+      let jsonContent = JSON.parse(content);
+      console.log("User:",jsonContent[0].author);
+      console.log("Date:",jsonContent[0].date);
+    for (i = 0; i < jsonContent.length; i++) { 
+    	//console.log(jsonContent[i].author)
+    	console.log("Entra")
+    	mod.appendFile("../MAINTAINERS","<Autor: "+jsonContent[i].author+">\n"+"Last Commit: "+jsonContent[i].date+" ("+jsonContent[i].message+")"+"\n"+" "+"\n", function(err){
+      	if(err){
+        	return console.log(err);
+      	}
+      	console.log("Modified file");
+
+  		});
+	}      
+      //console.log("Modified file");
   });
 
   if (error !== null){
