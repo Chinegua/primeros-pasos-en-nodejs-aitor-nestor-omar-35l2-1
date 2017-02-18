de128318e9dedfd7172122f4ff08033f1aceb2c4
Omar Mendo <omen.mesa20@gmail.com>
2017-02-18 19:04:22 +0000

    Modificando scripts

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec
 var mod = require('fs')
 
 child =exec ('git log',function(error,stdout,stderr){
-  console.log('stdout: ' + stdout)
+  
   mod.writeFile("../MAINTAINERS", stdout, function(err){
       if(err){
         return console.log(err);
@@ -10,4 +10,7 @@ child =exec ('git log',function(error,stdout,stderr){
       console.log("Modified file");
   });
 
-  
+  if (error !== null){
+     console.log('exec error: ' + error)
+  }
+});
