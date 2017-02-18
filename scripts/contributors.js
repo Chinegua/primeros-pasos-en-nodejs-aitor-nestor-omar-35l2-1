0a2bbe5f969cbc3db927a2a968321f7d412c390c
Omar Mendo <omen.mesa20@gmail.com>
2017-02-18 18:38:37 +0000

    Añadiendo contributors.js

---
+++ scripts/contributors.js
@@ -0,0 +1,14 @@
+var exec = require('child_process').exec
+var mod = require('fs')
+
+/*child =exec ('git log',function(err){
+  if(err){
+    return console.log(err);
+  }*/
+
+mod.writeFile("../MAINTAINERS", "mod", function(err){
+  if(err) {
+    return console.log(err);
+  }
+  console.log("Modified file");
+});
