8f9cbaf44a5b9a835da54a136e0206ab1eb651f2
Aitor Bernal <alu0100830064@ull.edu.es>
2017-02-18 23:53:56 +0000

    Añadiendo mejoras y solucionando errores en contributors

--- scripts/contributors.js
+++ scripts/contributors.js
@@ -2,8 +2,6 @@ let exec = require('child_process').exec
 let mod = require('fs')
 
 child =exec ('node git-log.js',function(error,stdout,stderr){
-  //console.log('stdout: ' + stdout)
-  //console.log(stdout)
   
   mod.writeFile("contributors.json", stdout, function(err){
       if(err){
@@ -14,13 +12,11 @@ child =exec ('node git-log.js',function(error,stdout,stderr){
       console.log("User:",jsonContent[0].author);
       console.log("Date:",jsonContent[0].date);
     for (i = 0; i < jsonContent.length; i++) { 
-    	//console.log(jsonContent[i].author)
-    	console.log("Entra")
+    	
     	mod.appendFile("../MAINTAINERS","<Autor: "+jsonContent[i].author+">\n"+"Last Commit: "+jsonContent[i].date+" ("+jsonContent[i].message+")"+"\n"+" "+"\n", function(err){
       	if(err){
         	return console.log(err);
       	}
-      	//console.log("Modified file");
 
   		});
 	}      
