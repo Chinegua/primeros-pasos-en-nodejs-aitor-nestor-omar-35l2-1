b7b3602da0f7c161d7a7cf764e5606bca1d039d2
Nestor <alu0100812503@ull.edu.es>
2017-02-19 15:13:03 +0000

    Mejorando script version

--- scripts/version.js
+++ scripts/version.js
@@ -7,19 +7,36 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const r2 = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-rl.question('Introduzca la version ', (answer) => {
-  // TODO: Log the answer in a database
-  let content = mod.readFileSync("../package.json");
-  let jsonContent = JSON.parse(content);
-  console.log("Version actual: "+jsonContent.version)
-  console.log("La version introducida es: "+answer);
-  jsonContent.version = answer
-  console.log(jsonContent);
-  jsonfile.writeFile("../package.json",jsonContent,{spaces: 2},(err) =>{
-    if(err){
-      return console.log(err);
-    }
-    rl.close();
-  });
+r2.question('Introduzca la opcion (m)odificar, (v)er ', (answer2) => {
+  switch(answer2) {
+
+  case 'm':
+            rl.question('Introduzca la version ', (answer) => {
+                let content = mod.readFileSync("../package.json");
+                let jsonContent = JSON.parse(content);
+                console.log("Version actual: "+jsonContent.version)
+                console.log("La version introducida es: "+answer);
+                jsonContent.version = answer
+                console.log(jsonContent);
+                jsonfile.writeFile("../package.json",jsonContent,{spaces: 2},(err) => {
+                    if(err){
+                    return console.log(err);
+                     }
+                    rl.close();
+                    });
+                });
+
+
+  case 'v':
+
+          const jsonContent = require('../package.json')
+          console.log("Version actual: " + jsonContent.version)
+
+  }
+  r2.close();
 });
